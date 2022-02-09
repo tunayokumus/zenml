61941b3c48bc886162c68d20a7d27de5a22c73f0
Michael Schuster <michael.schuster.ffb@googlemail.com>
2022-02-09 16:26:26 +0100

    Another try

--- mlflow_tests/test_mlflow.py
+++ mlflow_tests/test_mlflow.py
@@ -3,15 +3,6 @@ import sys
 
 
 def get_logger(logger_name: str) -> logging.Logger:
-    """Main function to get logger name,.
-
-    Args:
-      logger_name: Name of logger to initialize.
-
-    Returns:
-        A logger object.
-
-    """
     logger = logging.getLogger(logger_name)
     logger.setLevel(logging.INFO)
     console_handler = logging.StreamHandler(sys.stdout)
@@ -20,22 +11,20 @@ def get_logger(logger_name: str) -> logging.Logger:
     return logger
 
 
-def _init_logging() -> None:
-    logging.basicConfig(level=logging.INFO)
-    get_logger(__name__).debug("Logging initialized")
-
-
-def _step_2():
+def _import_mlflow():
     from mlflow import set_tracking_uri
     set_tracking_uri("")
+
+
+logging.basicConfig(level=logging.INFO)
+get_logger(__name__).debug("Logging initialized")
 
 
 def test_mlflow():
     assert "aria" != "random_cat"
 
-    _init_logging()
     get_logger(__name__).debug("This is a log")
-    _step_2()
+    _import_mlflow()
     get_logger(__name__).debug("This is another log")
     zero = 2-2
     assert zero == 0
